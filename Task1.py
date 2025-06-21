8db2cfa9efdbc74fba53636892c87f508b9e0ae9
Madhumitha-coding <maddy2008btsvt@gmail.com>
2025-06-21 01:09:28 -0700

    Add files via upload
    Task1 , Task2 code uploaded

---
+++ Task1.py
@@ -0,0 +1,8 @@
+a=int(input("ENTER THE FIST NUM:"))
+b=int(input("ENTER THE SECOND NUM:"))
+
+
+print("Addition:", a+b)
+print("Subtraction:" ,a-b)
+print("Multiplication:" , a*b)
+print("Division:" , a/b)
